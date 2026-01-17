1cc09b244f8b5ca705ce28f8332a4a51e370e37a
ds-gh2026 <daniel.shin@hashicorp.com>
2026-01-16 20:35:26 -0500

    Use_Case_1_5

--- main.tf
+++ main.tf
@@ -1,31 +1,38 @@
-# 1. Reads existing EC2 Instance
-# 2. Uses its attached Security Group
-# 3. Adds inbound rule: RDP (3389) allowing connection from anywhere
-# 4. Wiz Detecs exposure
-
-
-##########################################
-# Read existing EC2 Instance
-data "aws_instance" "existing" {
-  instance_id = "i-0ce66153d491a52f9"
+##### PLACEHOLDER VPC
+variable "vpc_id" {
+  type    = string
+  default = "vpc-00000000000000000000000"
 }
 
-##########################################
-# Select Security Group attached to the EC2 Instance
-locals {
-  security_group_id = sort(tolist(data.aws_instance.existing.vpc_security_group_ids))[0]
-}
+##### DEFINING A SECURITY GROUP IN TERRAFORM
+resource "aws_security_group" "insecure_rdp" {
 
-##########################################
-# Add an inbound rule allowing RDP from internet
-resource "aws_security_group_rule" "oepn_rdp" {
-  type              = "ingress"
-  security_group_id = "local.security_group_id"
-  
-  from_port    = 3389
-  to_port      = 3389
-  protocol     = "tcp"
-  cidr_blocks  = ["0.0.0.0/0"]
+  name        = "wiz-demo-insecure-rdp"
+  description = "Insecure RDP open to internet"
 
-  description  = "RDP open to the internet"
+  vpc_id      = var.vpc_id
+
+  ###### INBOUND RULE
+  ###### This is the security problem we want Wiz to detect
+  ingress {
+      
+      ###### RDP port for Windows
+      from_port = 3389
+      to_port   = 3389
+
+      ###### TCP Protocol
+      protocol  = "tcp"
+
+      ###### 0.0.0.0/0 "Open to internet"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ##### OUTBOUND RULE
+  ##### Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
---
+++ main_20260116.tf
@@ -0,0 +1,31 @@
+# 1. Reads existing EC2 Instance
+# 2. Uses its attached Security Group
+# 3. Adds inbound rule: RDP (3389) allowing connection from anywhere
+# 4. Wiz Detecs exposure
+
+
+##########################################
+# Read existing EC2 Instance
+data "aws_instance" "existing" {
+  instance_id = "i-0ce66153d491a52f9"
+}
+
+##########################################
+# Select Security Group attached to the EC2 Instance
+locals {
+  security_group_id = sort(tolist(data.aws_instance.existing.vpc_security_group_ids))[0]
+}
+
+##########################################
+# Add an inbound rule allowing RDP from internet
+resource "aws_security_group_rule" "oepn_rdp" {
+  type              = "ingress"
+  security_group_id = "local.security_group_id"
+  
+  from_port    = 3389
+  to_port      = 3389
+  protocol     = "tcp"
+  cidr_blocks  = ["0.0.0.0/0"]
+
+  description  = "RDP open to the internet"
+}
